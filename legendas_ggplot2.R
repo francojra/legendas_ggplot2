26b1198bbdefb356fe6f5b6a4f4d10a1fa3c779e
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-03-16 01:31:08 -0300

    Add files via upload

--- legendas_ggplot2.R
+++ legendas_ggplot2.R
@@ -19,7 +19,17 @@ p
 
 ## p + labs() altera o título da legenda.
 
-p + labs(fill = "Dose (mg)") # Se a legenda está ligada ao argumento fill
-p + labs(color = "Dose (mg)") # Se sua legenda está ligada ao argumento color
-
+## p + labs(fill = "Dose (mg)") # Se a legenda está ligada ao argumento fill
+## p + labs(color = "Dose (mg)") # Se sua legenda está ligada ao argumento color
 ## Assim por diante se sua legenda está ligada ao argumento 'shape', 'linetype', etc.
+
+## p + theme() altera a cor de fundo, borda da caixa da legenda, tamanho e cor da fonte, etc.
+
+## p + scale_x_discrete(limits = c=("2", "0.5", "1")) altera a ordem dos itens da legenda
+## se o objeto da legenda for discreto e também do eixo x.
+
+## p + scale_fill_discrete(name = "Dose (mg)", labels = c("A", "B", "C")) renomeia o
+## título da legenda e os rótulos de texto se o seu objeto da legenda for discreto.
+
+## p + theme(legend.title = elemente_blank()) remove o título da legenda.
+
