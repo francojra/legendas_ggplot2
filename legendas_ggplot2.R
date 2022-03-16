dafc5f0011d95fe7058993326fcbbe9a24be89e9
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-03-15 23:58:01 -0300

    Add files via upload

--- legendas_ggplot2.R
+++ legendas_ggplot2.R
@@ -14,3 +14,12 @@ p <- ggplot(ToothGrowth, aes(x = dose, y = len)) +
   geom_boxplot(aes(fill = dose))
 
 p
+
+# Principais funções para alterar legendas -------------------------------------------------------------------------------------------------
+
+## p + labs() altera o título da legenda.
+
+p + labs(fill = "Dose (mg)") # Se a legenda está ligada ao argumento fill
+p + labs(color = "Dose (mg)") # Se sua legenda está ligada ao argumento color
+
+## Assim por diante se sua legenda está ligada ao argumento 'shape', 'linetype', etc.
